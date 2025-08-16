ac6cf36089308570b4b9769e2519b7834997c6bc
MMzhong <106609962+hc353167950@users.noreply.github.com>
2025-08-16 16:58:00 +0800

    Create SupabaseConfig.kt

---
+++ app/src/main/java/com/example/xiaomaotai/SupabaseConfig.kt
@@ -0,0 +1,9 @@
+package com.example.xiaomaotai
+
+object SupabaseConfig {
+    // 替换为你的实际配置
+    const val SUPABASE_URL = "https://fcfqhnujbocxhccqalwa.supabase.co"
+    const val SUPABASE_ANON_KEY = "your_key"
+}
+
+
